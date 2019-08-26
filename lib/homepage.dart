74dea7cd99fadc706621d8876e2c6b0717e7645a
nghialh <nghialh@vietmap.vn>
2019-08-26 17:48:36 +0700

    + init
    Signed-off-by: nghialh <nghialh@vietmap.vn>

---
+++ lib/homepage.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/material.dart';
+import 'package:hoclaixe/listexam.dart';
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Ôn thi GPLX B2',
+          ),
+//            actions: <Widget>[
+//              IconButton(icon:  Icon(Icons.search),
+//                onPressed: (){},
+//              ),
+//            ],
+//        iconTheme: IconThemeData(color: Colors.black, opacity: 1,size: 50 )
+        ),
+        body: Column(
+          children: <Widget>[
+            Container(
+              height: 200,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage("images/365.png"), fit: BoxFit.fill)),
+            ),
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+//                    MaterialPageRoute(builder: (context) => CheckinPage(data[i],stream)));
+                          MaterialPageRoute(
+                              builder: (context) => ListExamPage()));
+                      print('123');
+                    },
+                    child: optionObject(OptionObject(
+                        iconOption: Icon(
+                          Icons.border_color,
+                          size: 40,
+                          color: Colors.lightBlue,
+                        ),
+                        textDescription: "Thi sát hạch")),
+                  ),
+                  optionObject(OptionObject(
+                      iconOption: Icon(
+                        Icons.book,
+                        size: 40,
+                        color: Colors.lightBlue,
+                      ),
+                      textDescription: "Học lý thuyết")),
+                  optionObject(OptionObject(
+                      iconOption: Icon(
+                        Icons.directions_bike,
+                        size: 40,
+                        color: Colors.lightBlue,
+                      ),
+                      textDescription: "Biển báo giao thông")),
+                ],
+              ),
+            )
+          ],
+        ));
+  }
+}
+
+class OptionObject {
+  OptionObject({
+    this.iconOption,
+    this.textDescription,
+  });
+  Icon iconOption;
+  String textDescription;
+}
+
+Widget optionObject(OptionObject option) {
+  return Container(
+    padding: EdgeInsets.all(5),
+    height: 110,
+    width: 125,
+//                    color: Colors.amber,
+    decoration: BoxDecoration(
+        border: Border.all(color: Colors.lightBlue, width: 2),
+        borderRadius: BorderRadius.all(Radius.circular(8))),
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        Container(
+          height: 40,
+//                          color: Colors.green,
+          child: Center(
+            child: option.iconOption,
+          ),
+        ),
+        Container(
+//        color: Colors.grey,
+          height: 50,
+          child: Center(
+            child: Text(
+              option.textDescription,
+              style: TextStyle(fontSize: 18),
+              textAlign: TextAlign.center,
+            ),
+          ),
+        )
+      ],
+    ),
+  );
+}
